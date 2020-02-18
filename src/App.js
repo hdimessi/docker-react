990b39b338165a2e546216ed562331a0d4ab0d07
hdimessi <hussein.dimessi@gmail.com>
2020-02-18 17:49:24 +0100

    changed main label

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi!
+          Hello world develop branch!
         </p>
         <a
           className="App-link"
